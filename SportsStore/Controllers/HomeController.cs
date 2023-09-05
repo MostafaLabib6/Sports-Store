179a0d5bcafa4e51effa07178fb3bd3941e6a5f9
MostafaLabib6 <mostafalabib510@gmail.com>
2023-09-05 22:13:00 +0300

    Create HomeController

---
+++ SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Data.Repositories;
+
+namespace SportsStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IStoreRepository _repository;
+
+        public HomeController(IStoreRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_repository.GetAll);
+        }
+    }
+}
