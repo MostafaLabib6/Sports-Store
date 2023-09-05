7b8fde421ba23bf245bfe47077aa865e73b9ef7b
MostafaLabib6 <mostafalabib510@gmail.com>
2023-09-05 22:10:28 +0300

    add migration

---
+++ SportsStore/Migrations/20230905173325_Update Price column.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SportsStore.Migrations
+{
+    public partial class UpdatePricecolumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                type: "decimal(8,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(6,2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                type: "decimal(6,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(8,2)");
+        }
+    }
+}
