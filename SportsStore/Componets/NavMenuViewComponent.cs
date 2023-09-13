0d132184ddbea83e51771bc99df52ff45623f947
MostafaLabib6 <mostafalabib510@gmail.com>
2023-09-13 19:26:14 +0300

    add navigation manu

---
+++ SportsStore/Componets/NavMenuViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Data.Repositories;
+
+namespace SportsStore.Componets;
+
+public class NavMenuViewComponent : ViewComponent
+{
+    private IStoreRepository? _storeRepository;
+
+    public NavMenuViewComponent(IStoreRepository? storeRepository)
+    {
+        _storeRepository = storeRepository;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        return View(
+            _storeRepository?.GetAll
+            .Select(x => x.Category)
+            .Distinct()
+            .OrderBy(x => x));
+    }
+}
