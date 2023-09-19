bd1d209d3e7a9d2c31a77b637e33fa811bfec25f
MostafaLabib6 <mostafalabib510@gmail.com>
2023-09-19 23:12:55 +0300

    add Url Extentions to back to continue Shopping

--- SportsStore/Infrastructure/UrlExtentions.cs
+++ SportsStore/Infrastructure/UrlExtentions.cs
@@ -1,13 +1,12 @@
-﻿namespace SportsStore.Infrastructure
-{
-    public static class UrlExtentions
-    {
-        public static string PathAndQuery(this HttpRequest request)
-        {
-            return request.QueryString.HasValue
-            ? $"{request.Path}{request.QueryString}"
-            : request.Path.ToString();
-        }
+﻿namespace SportsStore.Infrastructure;
 
+public static class UrlExtentions
+{
+    public static string PathAndQuery(this HttpRequest request)
+    {
+        return request.QueryString.HasValue
+        ? $"{request.Path}{request.QueryString}"
+        : request.Path.ToString();
     }
+
 }
