3842ec45fc4ca01beaeb963ec478388538b860d7
MostafaLabib6 <mostafalabib510@gmail.com>
2023-09-19 23:11:49 +0300

    add session extenstion to save user's cart products

---
+++ SportsStore/Infrastructure/SessionExtentions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.Json;
+
+namespace SportsStore.Infrastructure;
+
+public static class SessionExtentions
+{
+    public static void SetJson(this ISession session, string key, object value)
+    {
+        session.SetString(key, (string)JsonSerializer.Serialize(value));
+    }
+    public static T? GetJson<T>(this ISession session, string key)
+    {
+        var SessionData = session.GetString(key);
+        return SessionData == null 
+        ? default(T)
+        : JsonSerializer.Deserialize<T>(SessionData);
+    }
+}
