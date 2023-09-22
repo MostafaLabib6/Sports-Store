e94228c5ddd53b0d378e32c4d35cef5f0e11e0d6
MostafaLabib6 <mostafalabib510@gmail.com>
2023-09-22 22:06:27 +0300

    add cartService to save in data in session

---
+++ SportsStore/Models/CartService.cs
@@ -0,0 +1,38 @@
+﻿using SportsStore.Infrastructure;
+using System.Text.Json.Serialization;
+
+namespace SportsStore.Models
+{
+    public class CartService : Cart
+    {
+        [JsonIgnore]
+        public ISession? Session { get; set; }
+        public static Cart GetCart(IServiceProvider services)
+        {
+            ISession? session = services.GetRequiredService<IHttpContextAccessor>()
+            .HttpContext?.Session;
+            CartService cart = session?.GetJson<CartService>("Cart")
+            ?? new CartService();
+            cart.Session = session;
+            return cart;
+        }
+        public override void AddItem(Product product, int quantity)
+        {
+            base.AddItem(product, quantity);
+            Session?.SetJson("Cart", this);
+        }
+        public virtual void RemoveLine(Product product)
+        {
+            base.RemoveLine(product);
+            Session?.SetJson("Cart",this);
+        }
+
+        public virtual void Clear()
+        {
+            base.Clear();
+            Session?.Remove("Cart");
+
+        }
+
+    }
+}
