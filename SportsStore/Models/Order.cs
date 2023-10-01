5d67ad4511739253fcc7ffd4d0282cbd06ee9aba
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-01 13:54:07 +0300

    Create Order.cs

---
+++ SportsStore/Models/Order.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SportsStore.Models;
+
+public class Order
+{
+    [BindNever]
+    public int OrderId { get; set; }
+    public ICollection<CartLine> Lines { get; set; } = new List<CartLine>();
+
+    [Required(ErrorMessage ="Please Enter Name")]
+    public string? Name { get; set; }
+    
+    [Required(ErrorMessage ="Please Enter Address")]
+    public string? AddressLine { get; set; }
+    
+    [Required(ErrorMessage ="Please Enter your City")]
+    public string? City { get; set; }
+
+    [Required(ErrorMessage ="Please Enter your Region")]
+    public string? Region { get; set; }
+
+    [Required(ErrorMessage ="Please Enter your Postal Code"), DisplayName("Zip Code")]
+    public string? PostalCode { get; set; }
+
+    [Required(ErrorMessage ="Please Enter your Phone Number"), MaxLength(11), MinLength(11) ]
+    public string? Phone { get; set; }
+
+    public bool? GiftCard { get; set; } = false;
+
+
+
+
+}
