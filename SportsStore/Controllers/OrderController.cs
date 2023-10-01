a7e10a728dde8a3406a50c43ab15bb39dab165c6
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-01 13:48:34 +0300

    add order controller

---
+++ SportsStore/Controllers/OrderController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Data.Repositories;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers;
+
+public class OrderController : Controller
+{
+    private readonly IOrderRepository _context;
+    private readonly Cart _cart;
+    public OrderController(IOrderRepository context, Cart cart)
+    {
+        _context = context;
+        _cart = cart;
+    }
+
+    public IActionResult Checkout()
+    {
+        return View(new Order());
+    }
+    [HttpPost]
+    public IActionResult Checkout(Order order)
+    {
+        if (order is null || order.Lines.Count > 1)
+            ModelState.AddModelError("", "Sorry your Cart is Empty!!!");
+        if (ModelState.IsValid)
+        {
+            order!.Lines = _cart.Lines.ToArray();
+            _context.SaveChanges(order!);
+            _cart.Clear();
+            return RedirectToPage("/Complete", order.OrderId);
+
+        }
+        else
+            return View();
+    }
+
+}
