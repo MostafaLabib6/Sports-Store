162c1dd013f022e963524c822d49c4b17003db6d
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-01 13:53:50 +0300

    update dbcontext class
    implement order pattern

---
+++ SportsStore/Data/Repositories/OrderRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
+namespace SportsStore.Data.Repositories;
+
+public class OrderRepository : IOrderRepository
+{
+    private readonly StoreDbContext _dbContext;
+
+    public OrderRepository(StoreDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public IQueryable<Order> Orders => _dbContext.orders.Include(o => o.Lines).ThenInclude(o => o.Product);
+
+    void IOrderRepository.SaveChanges(Order order)
+    {
+        _dbContext.AttachRange(order.Lines.Select(p => p.Product));
+        _dbContext.orders.Add(order);
+        _dbContext.SaveChanges();
+    }
+}
