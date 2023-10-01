7e228c5c29755e7903dc42e53cc76e3465012f39
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-01 13:48:55 +0300

    apply Repository patten

---
+++ SportsStore/Data/Repositories/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿using SportsStore.Models;
+
+namespace SportsStore.Data.Repositories;
+
+public interface IOrderRepository
+{
+    public IQueryable<Order> Orders { get;}
+    public void SaveChanges (Order order);
+}
