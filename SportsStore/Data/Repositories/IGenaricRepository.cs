ab7dfcf03b920b2f76c0ece94b2f3d67de82b3db
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-07 18:46:45 +0300

    Update IGenaricRepository.cs
    Change return type of Getall to be querable

--- SportsStore/Data/Repositories/IGenaricRepository.cs
+++ SportsStore/Data/Repositories/IGenaricRepository.cs
@@ -5,13 +5,13 @@ namespace SportsStore.Data.Repositories
 {
     public interface IGenaricRepository<T> where T : class
     {
-        public IEnumerable<T> GetAll();
+        public IQueryable<T> GetAll();
         public T Get(Expression<Func<T, bool>> filter);
 
         public void Update(T entity);
         public void Delete(T entity);
         public void Add(T entity);
         public void RemoveRange(IEnumerable<T> entities);
-        public void SaveChanges(T entity);
+        public void SaveChanges();
     }
 }
