b5228222c6bd1863c2996b7825e42811331a7b2b
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-07 18:47:58 +0300

    Update Product.cs

--- SportsStore/Models/Product.cs
+++ SportsStore/Models/Product.cs
@@ -8,15 +8,16 @@ namespace SportsStore.Models
     {
         public long ProductId { get; set; } 
         
-        [Column(TypeName ="varchar(100)"),Required]
+        [Column(TypeName ="varchar(100)"),Required(ErrorMessage ="Please Enter Product Name")]
         public string Name { get; set; }=String.Empty;
         
-        [Column(TypeName = "decimal(8,2)")]
+        [Column(TypeName = "decimal(8,2)"),Range(0.01,double.MaxValue,ErrorMessage ="Price must be Valuable")]
         public decimal Price { get; set; }
         
-
         [Column(TypeName ="varchar(300)")]
         public string Description { get; set; } = String.Empty;
+        
+        [Column(TypeName ="varchar(300)"),Required(ErrorMessage ="Please Enter Category")]
         public string Category { get; set; }=string.Empty;
 
     }
